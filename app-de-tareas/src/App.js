2a891645074d57bd1427afdbe9c90789552b223a
Alberto Encarnación Jerez <153152111+4Alberto@users.noreply.github.com>
2024-03-21 02:10:38 -0400

    Add files via upload

---
+++ app-de-tareas/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import integradc2 from './imagen/dc2.png';
+import ListaDeTareas from './componentes/ListaDeTareas';
+
+function App() {
+  return (
+    <div className='aplicacion-tareas'>
+      <div className='logo-contenedor'>
+        <img
+          src={integradc2} className='integralogo' alt='dc2' />
+      </div>
+      <div className='tareas-lista-principal'>
+        <h1>Mis Tareas</h1>
+        <ListaDeTareas />
+      </div>
+    </div>
+  );
+}
+
+export default App;
